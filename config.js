94e80ba5c9e60ac69412f9e89cfc63be14fc9701
Samciu <samciu@163.com>
2021-03-31 17:13:51 +0800

    修改配置

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 
 export default {
-    // baseUrl: "https://zy-api.wxthe.com",
-    // Appid: 'zy36aab89b0773212d'
-    baseUrl: "http://zhongyong.wxneed.com",
+    baseUrl: "https://zy-api.wxthe.com",
     Appid: 'zy36aab89b0773212d'
 }
