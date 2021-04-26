13c6689fa2eedaac1daea36a7e83e45a658946c9
代小松 <delfws@qq.com>
2021-04-26 22:47:34 +0800

    fix:首页轮播图banner点击报错

--- pages/takeout/takeout.vue
+++ pages/takeout/takeout.vue
@@ -136,6 +136,8 @@ export default {
           "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-5421f5a2-25ab-411d-b114-90177d80d0eb/1fa7c836-b3cc-44ba-84b0-6e6e9188244b.jpg",
       };
     },
+	
+	handleClickBanner() { },
   },
 };
 </script>
