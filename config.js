b438f4fcb7006365f89173bd70354cbfb7869e8f
Samciu <samciu@163.com>
2021-07-01 15:43:50 +0800

    update config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 export default {
     baseUrl: "https://zy-api.wxthe.com",
-    Appid: 'zy36aab89b0773212d',
+    Appid: 'zy36aab89b0773212d', // 你的众佣appid，改完记得保存
     usePlugin: true // 是否使用支付插件。个人主体无法使用，请配置为false
 }
